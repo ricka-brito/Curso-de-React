c81594bb0bab0c31c35864a69083216cedeb11c9
Rick <rickabrito@gmail.com>
2024-02-05 14:23:51 -0300

    first commit

---
+++ src/components/TextInput/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.css';
+
+export const TextInput = ({ searchValue, handleChange }) => {
+  return (
+    <input 
+      className='text-input'
+      type='search'
+      onChange={handleChange}
+      value={searchValue}
+      placeholder='Type your search'
+    />
+  )
+}
